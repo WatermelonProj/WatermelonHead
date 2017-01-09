a184bbd67c8611c7689f27c08725ad13707eba5d
lucasszmt <lucasszmt@gmail.com>
2017-01-09 16:56:15 -0200

    base do form Cadastro de Alimentos]

--- private/app_data/resources/views/alimentos/alimentoForm.blade.php
+++ private/app_data/resources/views/alimentos/alimentoForm.blade.php
@@ -1,10 +1,30 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="form-group">
-    {!! Form::open(['route' => 'alimentos.teste']) !!}
-        {{!! Form::label('email', 'E-Mail Address', ['class' => 'awesome']); !!}}
 
-    {!! Form::close() !!}
+    {{--Cabeçalho--}}
+    <div class="row">
+        <div class="col-lg-12">
+            <h1>Alimento</h1>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Cadastro de Alimentos</div>
+                <div class="panel-body">
+                    {{--Form--}}
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
+
+<ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
