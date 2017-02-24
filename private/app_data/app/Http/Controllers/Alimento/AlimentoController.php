35cc1e9a6247215ca6d46baed23b52512c2f6665
lucasszmt <lucasszmt@gmail.com>
2017-02-24 16:51:19 -0300

    FAzendo Validação

--- private/app_data/app/Http/Controllers/Alimento/AlimentoController.php
+++ private/app_data/app/Http/Controllers/Alimento/AlimentoController.php
@@ -37,7 +37,7 @@ class AlimentoController extends Controller
     }
 
     /**
-     * Salva as informa��es b�sicas de um alimento e redireciona para a cria��o de medidas caseiras
+     * Salva as informações básicas de um alimento e redireciona para a criação de medidas caseiras
      * se o alimento a possuir
      *
      * @param  \Illuminate\Http\Request $request
@@ -45,43 +45,55 @@ class AlimentoController extends Controller
      */
     public function store(Request $request)
     {
-        // Criando um alimento
-        $alimento = new Alimento();
-        $alimento->descricaoAlimento = $request->descricaoAlimento;
-        $alimento->grupoPiramide()->associate(GrupoPiramide::find($request->idGPiramide));
-        $alimento->grupoAlimentar()->associate(GrupoAlimentar::find($request->idGAlimentar));
-        $alimento->idTACO = $request->idTACO;
-        $alimento->save();
+        dump($request);
 
-        //criando a rela��o do alimento para com o nutriente
-        $keys = array_keys($request->toArray());
-        for ($i = 6; $i < count($request->toArray()); $i++) {
-            $nutrienteAlm = new NutrienteAlimento();
-            $nutrienteAlm->alimento()->associate($alimento);
-            $nutriente = Nutriente::where('nomeNutriente', str_replace('_', ' ', $keys[$i]))->first();
-            $nutrienteAlm->nutriente()->associate($nutriente);
-            $nutrienteAlm->qtde = ($request[$keys[$i]] > 0 ? $request[$keys[$i]] : 'NA');
-            $nutrienteAlm->save();
-        }
-
-        //criando a rela��o do alimento com suas respectivas medidas caseiras
-        for ($i = 0; $i < count($request->medidas_caseiras); $i++) {
-            $medidaCaseira = new AlimentoMedidaCaseira();
-            $medidaCaseira->alimento()->associate($alimento); //indexando com o alimento
-            $tipoMedida = TipoMedidaCaseira::where('idTMCaseira', $request->medidas_caseiras[$i])->first();
-            $medidaCaseira->tipoMedidaCaseira()->associate($tipoMedida); //indexando com o tipo de medida caseira
-            $unidadeMedida = UnidadeMedida::where('idUnidade', 2)->first();
-            $medidaCaseira->unidadeMedida()->associate($unidadeMedida); // associando com a unidade de medida em g por padr�o
-            $medidaCaseira->save();
-        }
-
-        //Caso o alimento possua medidas caseiras, redireciona para a pagina de informações das mesmas
-        if (count($request->medidas_caseiras) > 0) {
-            return redirect()->route('alimentos.createMedida', ['id' => $alimento->idAlimento])
-                ->with('status', 'Alimento criado com sucesso, insira os valores das mediads caseiras');
-        } else {
-            return redirect()->route('alimentos')->with('status', 'Alimento criado com sucesso!');
-        }
+        //validação
+//        $this->validate($request,
+//            [
+//                'descricaoAlimento' => 'required',
+//                'idGpiramide' => 'required',
+//                'idTACO' => 'required',
+//            ]
+//
+//        );
+//
+//        // Criando um alimento
+//        $alimento = new Alimento();
+//        $alimento->descricaoAlimento = $request->descricaoAlimento;
+//        $alimento->grupoPiramide()->associate(GrupoPiramide::find($request->idGPiramide));
+//        $alimento->grupoAlimentar()->associate(GrupoAlimentar::find($request->idGAlimentar));
+//        $alimento->idTACO = $request->idTACO;
+//        $alimento->save();
+//
+//        //criando a relaï¿½ï¿½o do alimento para com o nutriente
+//        $keys = array_keys($request->toArray());
+//        for ($i = 6; $i < count($request->toArray()); $i++) {
+//            $nutrienteAlm = new NutrienteAlimento();
+//            $nutrienteAlm->alimento()->associate($alimento);
+//            $nutriente = Nutriente::where('nomeNutriente', str_replace('_', ' ', $keys[$i]))->first();
+//            $nutrienteAlm->nutriente()->associate($nutriente);
+//            $nutrienteAlm->qtde = ($request[$keys[$i]] > 0 ? $request[$keys[$i]] : 'NA');
+//            $nutrienteAlm->save();
+//        }
+//
+//        //criando a relaï¿½ï¿½o do alimento com suas respectivas medidas caseiras
+//        for ($i = 0; $i < count($request->medidas_caseiras); $i++) {
+//            $medidaCaseira = new AlimentoMedidaCaseira();
+//            $medidaCaseira->alimento()->associate($alimento); //indexando com o alimento
+//            $tipoMedida = TipoMedidaCaseira::where('idTMCaseira', $request->medidas_caseiras[$i])->first();
+//            $medidaCaseira->tipoMedidaCaseira()->associate($tipoMedida); //indexando com o tipo de medida caseira
+//            $unidadeMedida = UnidadeMedida::where('idUnidade', 2)->first();
+//            $medidaCaseira->unidadeMedida()->associate($unidadeMedida); // associando com a unidade de medida em g por padrï¿½o
+//            $medidaCaseira->save();
+//        }
+//
+//        //Caso o alimento possua medidas caseiras, redireciona para a pagina de informaÃ§Ãµes das mesmas
+//        if (count($request->medidas_caseiras) > 0) {
+//            return redirect()->route('alimentos.createMedida', ['id' => $alimento->idAlimento])
+//                ->with('status', 'Alimento criado com sucesso, insira os valores das mediads caseiras');
+//        } else {
+//            return redirect()->route('alimentos')->with('status', 'Alimento criado com sucesso!');
+//        }
 
     }
 
--- private/app_data/resources/views/alimentos/alimentosCriacao.blade.php
+++ private/app_data/resources/views/alimentos/alimentosCriacao.blade.php
@@ -5,7 +5,15 @@
 @endsection
 
 @section('content')
-
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     {{--Cabeçalho--}}
     <div class="row">
         <div class="col-lg-12">
@@ -30,7 +38,7 @@
                     <div class="form-group item">
                         {!! Form::label('descricaoAlimento', 'Alimento', ['class'=>'control-label col-md-3 col-sm-3 col-xs-12']) !!}
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            {!! Form::text('descricaoAlimento', null, ['class'=>'form-control col-md-7 col-xs-12', 'required'=>'required']) !!}
+                            {!! Form::text('descricaoAlimento', null, ['class'=>'form-control col-md-7 col-xs-12', ]) !!}
                         </div>
                     </div>
                     <div class="form-group ">
