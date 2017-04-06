023f7f938ed4b147be68b636c4ec579c4be644de
lucasszmt <lucasszmt@gmail.com>
2017-04-06 10:21:25 -0300

    Home arrumada

--- private/app_data/resources/views/home.blade.php
+++ private/app_data/resources/views/home.blade.php
@@ -2,8 +2,32 @@
 
 @section('content')
 
-    <div class="row">
-        <h1>Teste</h1>
+    <div class="row tile_count">
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-user"></i> Total de Usuários</span>
+            <div class="count">35</div>
+            <span class="count_bottom"><i class="green">4% </i> Desde o mês passado</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-home"></i> Total de Escolas</span>
+            <div class="count">120</div>
+            <span class="count_bottom"><i class="green">3% </i> Desde o mês passado</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-apple"></i> Total de Alimentos Cadastrados</span>
+            <div class="count">31</div>
+            <span class="count_bottom"><i class="green">12% </i> Desde o mês passado</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-cutlery"></i> Total de Receitas Cadastradas</span>
+            <div class="count">79</div>
+            <span class="count_bottom"><i class="green">9% </i> Desde o mês passado</span>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-spoon"></i> Total de Cardápios Ativos</span>
+            <div class="count">55</div>
+            <span class="count_bottom"><i class="green">4% </i> Desde semana passada</span>
+        </div>
     </div>
 
 @endsection
