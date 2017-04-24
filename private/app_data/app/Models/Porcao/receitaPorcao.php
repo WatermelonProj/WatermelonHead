90e62362813eb3bc69d39bfe4cf20416ee70a1f8
Lucas Szeremeta <lucasszmt@gmail.com>
2017-04-24 00:19:53 -0300

    Modelos de Porção criados

---
+++ private/app_data/app/Models/Porcao/receitaPorcao.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Porcao;
+
+use Illuminate\Database\Eloquent\Model;
+
+class receitaPorcao extends Model
+{
+    protected $table = 'receita_porcao';
+}
---
+++ private/app_data/app/Models/Porcao/tipoPorcao.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Porcao;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tipoPorcao extends Model
+{
+    protected $table = 'tipo_porcao';
+}
